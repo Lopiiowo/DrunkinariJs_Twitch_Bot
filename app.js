cf36d62144cb47d16cda01a9627bf4491ac38659
AkinariHex <akinari@outlook.it>
2017-12-30 11:07:56 +0100

    Some changes

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 var tmi = require('tmi.js');
+var login = require('./login.js');
+
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: `${login.username}`,
+        password: `${login.password}`
+    },
+    channels: ["#akinari_live"]
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on("chat", function (channel, userstate, message, self) {
+    if (message == "!bad"){
+        client.action("#akinari_live", "you're noob!")
+    };
+    if (self) return;
+    // Do your stuff.
+});
