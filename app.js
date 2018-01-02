152a5e6cd10ccb8b876edea31b12fbb84de320ea
AkinariHex <akinari@outlook.it>
2018-01-02 11:55:19 +0100

    Little Fix for commands

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 var fs = require('fs');
 var tmi = require('tmi.js');
 var login = require('./login.js');
-
 const commandFiles = fs.readdirSync('./commands');
-
-for (const file of commandFiles) {
-    var command = require(`./commands/${file}`);
-    }
 
 var options = {
     options: {
@@ -26,11 +21,10 @@ var client = new tmi.client(options);
 client.connect();
 
 client.on("chat", function (channel, userstate, message, self) {
-    if (message != command.name) return;
-    
-    if (message == command.name) {
-        client.action(command.channel, command.text);
+    for (const file of commandFiles) {
+        var command = require(`./commands/${file}`);
+        if (message == command.name) {
+            client.action(command.channel, command.text);
+        }
     }
-
-    if (self) return;
 });
